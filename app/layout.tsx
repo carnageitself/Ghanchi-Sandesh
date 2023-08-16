ca1f50c0b711031ed00e807763940322906a5043
GODCarNageOP <yashvharale@gmail.com>
2023-08-17 00:17:37 +0530

    components created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from './components/navbar'
+import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+    <Navbar/>
+   <div className="div">{children}</div>
+    <Footer/>
+    </>
   )
 }
