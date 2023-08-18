ed459ef2efd3a9cceb773d0b0b192cb078faf546
Akash Kashyap <Aakashdev24@gmail.com>
2023-08-18 05:53:45 +0530

    community-page-added

---
+++ app/components/CommunityData.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Img from '/public/santhaye-2.jpeg'
+import Search from './Search'
+import Image from 'next/image'
+
+const CommunityData = () => {
+  return (
+    <div className="flex    flex-col items-center">
+      <div className='w-[99.09vw] min-h-[70vh] bg-gray-100 flex    flex-col items-center'>
+      <div className=" img-div  w-full  h-[400px]  overflow-hidden ">
+<Image src={Img} alt='image' className='w-full' />
+      </div>
+      <div className=" w-full max-w-[750px] relative bottom-[35px] ">
+
+      <Search/>
+      </div>
+<span className='text-start text-[32px] font-bold'>Home Director</span>
+      </div>
+    </div>
+  )
+}
+
+export default CommunityData
