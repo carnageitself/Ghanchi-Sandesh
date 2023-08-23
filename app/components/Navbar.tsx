94761785b8f642650346b75aa9b4debdd69f6f54
Namansingh98 <naman.rathore98@outlook.com>
2023-08-24 00:31:45 +0530

    nav responsive done toggle not working

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,30 +1,253 @@
 "use client"
 import Image from "next/image";
-import React, { useState } from "react";
+import { useState } from "react";
 import Img from "/public/logo.png";
 import { Divide as Hamburger } from 'hamburger-react'
 
 const Navbar = () => {
 
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
 
+  const [navbar, setNavbar] = useState(false);
+
+  // const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  
+  // const toggleMobileMenu = () => {
+    
+  //   setIsMobileMenuOpen(!isMobileMenuOpen);
+    
+  // };
   return (
-    <div className="navbar w-full bg-[#C10A39] text-white shadow-xl">
-      <div className="container mx-auto flex items-center justify-between p-[20px] h-[72px] top-[30px]">
-        <div className="logo flex items-center">
-        <Image src={Img} className="" width={150} height={100} alt="logo" />
-        </div>
-        <div className="nav gap-12 flex items-center justify-between text-xl font-medium">
-          <span className="cursor-pointer">टॉप न्यूज़</span>
-          <span className="cursor-pointer">सामाजिक</span>
-          <span className="cursor-pointer">धार्मिक</span>
-          <span className="cursor-pointer">शेक्शनिक</span>
-          <span className="cursor-pointer">अन्य</span>
-        </div>
-        <div className="menu"><Hamburger size={22}/></div>
-      </div>
-    </div>
+    // <div className="navbar w-full bg-[#C10A39] text-white shadow-xl">
+    //   <div className="container mx-auto flex items-center justify-between p-[20px] h-[72px] top-[30px]">
+    //     <div className="logo flex items-center">
+    //     <Image src={Img}  width={150} height={100} alt="logo" />
+    //     </div>
+    //     <div className="nav gap-12 flex items-center justify-between text-xl font-medium">
+    //       <span className="cursor-pointer">टॉप न्यूज़</span>
+    //       <span className="cursor-pointer">सामाजिक</span>
+    //       <span className="cursor-pointer">धार्मिक</span>
+    //       <span className="cursor-pointer">शेक्शनिक</span>
+    //       <span className="cursor-pointer">अन्य</span>
+    //     </div>
+    //     <div className="menu">
+    //       <Hamburger size={22}/>
+    //       </div>
+    //   </div>
+    // </div>
+
+
+
+    <>
+      <nav className="w-full bg-[#C10A39] shadow">
+            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+                <div>
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                        <a href="/">
+                        <Image src={Img}  width={150} height={100} alt="logo" />
+                        </a>
+                        <div className="md:hidden">
+                            <button
+                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                onClick={() => setNavbar(!navbar)}
+                            >
+                                {navbar ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6 text-white"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6 text-white"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div
+                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                            navbar ? "block" : "hidden"
+                        }`}
+                    >
+                        <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="#">टॉप न्यूज़</a>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="/">सामाजिक</a>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="/">धार्मिक</a>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="/">शेक्शनिक</a>
+                            </li>
+                            <li className="text-white hover:text-indigo-200">
+                                <a href="/">अन्य</a>
+                            </li>
+                        </ul>
+
+                        {/* <div className="mt-3 space-y-2 lg:hidden md:inline-block">
+                    <a
+                        href="/"
+                        className="inline-block w-full px-4 py-2 text-center text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
+                    >
+                        Sign in
+                    </a>
+                    <a
+                        href="/"
+                        className="inline-block w-full px-4 py-2 text-center text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
+                    >
+                        Sign up
+                    </a>
+                </div> */}
+                    </div>
+                </div>
+                {/* <div className="hidden space-x-2 md:inline-block">
+                    <a
+                        href="/"
+                        className="px-4 py-2 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
+                    >
+                        Sign in
+                    </a>
+                    <a
+                        href="/"
+                        className="px-4 py-2 text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
+                    >
+                        Sign up
+                    </a>
+                </div> */}
+            </div>
+        </nav>
+    </>
+    // <nav className="bg-black/30 p-4 content-center">
+    //   <div
+    //     className="container mx-auto flex justify-between items-center"
+    
+    
+    //   >
+    //     <div className="text-white font-bold text-xl">
+    //       <Image src={Img}  width={150} height={100} alt="logo" className="curser-pointer"/>
+    //     </div>
+    //     <div className="hidden md:flex space-x-4">
+    //       <a
+    //         href="#"
+    //         className="text-white hover:text-gray-800 hover:bg-slate-50 p-2 rounded-md  transition-all"
+    //       >
+    //         Home
+    //       </a>
+    //       <a
+    //         href="/admission"
+    //         className="text-white   hover:text-gray-800 hover:bg-slate-50 p-2 rounded-md transition-all"
+    //       >
+    //         Admission
+    //       </a>
+    //       <a
+    //         href="/topcolleges"
+    //         className="text-white  hover:text-gray-800 hover:bg-slate-50 p-2 rounded-md transition-all"
+    //       >
+    //         Top College
+    //       </a>
+
+    //       <a
+    //         href="/about"
+    //         className="text-white hover:text-gray-800 hover:bg-slate-50 p-2 rounded-md transition-all"
+    //       >
+    //         About us
+    //       </a>
+    //       <a
+    //         href="/contact"
+    //         className="text-white hover:text-gray-800 hover:bg-slate-50 p-2 rounded-md transition-all"
+    //       >
+    //         Contact us
+    //       </a>
+         
+    //     </div>
+
+    //     <div className="md:hidden">
+    //       {/* Mobile menu button */}
+    //       <button className="text-white" onClick={toggleMobileMenu} >
+    //         <svg
+    //           xmlns="http://www.w3.org/2000/svg"
+    //           className="h-6 w-6"
+    //           fill="none"
+    //           viewBox="0 0 24 24"
+    //           stroke="currentColor"
+    //         >
+    //           <path
+    //             strokeLinecap="round"
+    //             strokeLinejoin="round"
+    //             strokeWidth="2"
+    //             d="M4 6h16M4 12h16m-7 6h7"
+    //           ></path>
+    //         </svg>
+    //       </button>
+    //     </div>
+    //   </div>
+    //   {/* Mobile menu */}
+      
+    //     {isMobileMenuOpen && (
+    //       <div
+            
+    //         className="md:hidden mt-2 space-y-2 text-center"
+    //       >
+    //         <a href="#" className="block text-white hover:text-gray-300">
+    //           Home
+    //         </a>
+    //         <a
+    //           href="/admission"
+    //           className="block text-white hover:text-gray-300"
+    //         >
+    //           Admission
+    //         </a>
+    //         <a
+    //           href="/topcollege"
+    //           className="block text-white hover:text-gray-300"
+    //         >
+    //           Top College
+    //         </a>
+    //         <a
+    //           href="/about"
+    //           className="block text-white hover:text-gray-300"
+    //         >
+    //           About us
+    //         </a>
+    //         <a
+    //           href="/contact"
+    //           className="block text-white hover:text-gray-300"
+    //         >
+    //           Contact us
+    //         </a>
+       
+    //       </div>
+    //     )}
+     
+    // </nav>
+
   )
+
 }
 
 export default Navbar;
