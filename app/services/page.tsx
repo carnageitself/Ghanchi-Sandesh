f16af97ff224eb38c3494ef64220b9b1de85c13c
GODCarNageOP <yashvharale@gmail.com>
2023-08-24 19:33:03 +0530

    services-page-added

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const page = () => {
+const Services = () => {
   return (
-    <div>page</div>
+   <div className="services bg-gray-100 w-full h-full">
+    <div className="title">
+      <h1 className='text-[#B80433] text-5xl font-bold my-10 ml-10'>Social Works</h1>
+    </div>
+    <div className="links flex items-center justify-center gap-5">
+      <h1 className='border border-[#C10A39] cursor-pointer text-[#C10A39] bg-white p-3 px-10 rounded-full text-2xl font-medium hover:bg-[#C10A39] hover:text-white'>Samaj bhavan</h1>
+      <h1 className='border  border-[#C10A39] cursor-pointer text-[#C10A39] bg-white p-3 px-10 rounded-full text-2xl font-medium hover:bg-[#C10A39] hover:text-white'>Samaj bhavan</h1>
+      <h1 className='border  border-[#C10A39] cursor-pointer text-[#C10A39] bg-white p-3 px-10 rounded-full text-2xl font-medium hover:bg-[#C10A39] hover:text-white'>Samaj bhavan</h1>
+      <h1 className='border  border-[#C10A39] cursor-pointer text-[#C10A39] bg-white p-3 px-10 rounded-full text-2xl font-medium hover:bg-[#C10A39] hover:text-white'>Samaj bhavan</h1>
+    </div>
+   </div>
   )
 }
 
-export default page
+export default Services
