c85b4abf2790d7dd348de096fec1d05cb206d112
Namansingh98 <naman.rathore98@outlook.com>
2023-08-27 13:40:24 +0530

    social page done

--- app/components/ServiceFeed.tsx
+++ app/components/ServiceFeed.tsx
@@ -3,10 +3,13 @@ import ServiceCard from './ServiceCard'
 
 const ServiceFeed = () => {
   return (
-    <div className="feed flex items-center justify-between mx-10 gap-20">
+  
+     <>
+    
+
+
       <ServiceCard/>
-      <ServiceCard/>
-      </div>
+      </>
   )
 }
 
