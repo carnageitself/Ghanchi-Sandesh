eeb70d41406494d0ae5a443cc8a4dd97abb91fb3
GODCarNageOP <yashvharale@gmail.com>
2023-08-27 13:12:56 +0530

    news-page-added

---
+++ app/components/MagazineData.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import img from "../../public/img.jpeg"
+import React from 'react'
+
+const MagazineData = () => {
+  return (
+  
+      <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+        <article className="overflow-hidden rounded-lg shadow-lg">
+          <div className="img__main__container ">
+            <a href="#">
+              <Image
+                alt="Placeholder"
+                className="block h-[550px] w-full p-5  img__radius"
+                src={img}
+              />
+            </a>
+          </div>
+        </article>
+      </div>
+  )
+}
+
+export default MagazineData
--- app/magazine/page.tsx
+++ app/magazine/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import MagazineData from "../components/MagazineData";
+import {IoIosCall} from "react-icons/io"
 
-const page = () => {
+const Magazine = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="magazine w-full bg-gray-100">
+      <div className="title">
+        <h1 className="text-[#B80433] text-5xl font-bold my-10 ml-10">News</h1>
+      </div>
+      <div className="container my-12 mx-auto px-4 md:px-12">
+    <div className="flex flex-wrap -mx-1 lg:-mx-4">
+      <MagazineData />
+      <MagazineData />
+      <MagazineData />
+      <MagazineData />
+      <MagazineData />
+      <MagazineData />
+      </div>
+      </div>
+      <div className="contact w-full">
+      <div className="container flex flex-col h-72 bg-white items-center justify-center gap-3">
+       <h1 className="text-5xl font-semibold text-[#C10A39]">To advertise in ghanchi sandesh contact</h1>
+       <h1 className="text-5xl font-semibold text-black">Dinesh Kumar</h1>
+       <div className="flex items-center gap-2">
+       <IoIosCall size={40}/>
+         
+       <h1 className="text-5xl font-medium text-black">+123456789</h1>
+       </div>
+      </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default Magazine;
