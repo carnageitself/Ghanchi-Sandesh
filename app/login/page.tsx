13292befad2ec67cc30fd54f5e4c543799e4f44d
GODCarNageOP <yashvharale@gmail.com>
2023-09-02 12:34:26 +0530

    login-updated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
-
-export default Login
-=======
 import React from "react";
 import Login from "../components/auth/Login";
 
@@ -21,4 +10,3 @@ const page = () => {
 };
 
 export default page;
->>>>>>> af4ed8d7dc60a5a034b8f72fef63c7b26491ff44
