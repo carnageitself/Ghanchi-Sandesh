ebfb8958b716e74cdf90038462943c7a08120c03
Akash Kashyap <Aakashdev24@gmail.com>
2023-09-04 20:53:34 +0530

    fixed-fetch

--- app/hooks/useAuth.tsx
+++ app/hooks/useAuth.tsx
@@ -1,51 +1,50 @@
 import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation'; // Import useRouter from Next.js for routing
-import Cookies from 'js-cookie';
+import { usePathname } from 'next/navigation';
 import API_URL from '../url';
 import { useUserDetails } from './useUserDetails';
 
 interface AuthState {
   authenticated: boolean | null;
-  user: any | null; // Change the type to match your user data structure
+  user: any | null;
   loading: boolean;
   error: Error | null;
 }
 
 export function useAuth(): AuthState {
   const [authenticated, setAuthenticated] = useState<boolean | null>(null);
-  const [user, setUser] = useState<any | null>(null); // Change the type to match your user data structure
+  const [user, setUser] = useState<any | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<Error | null>(null);
-  const pathname = usePathname(); // Initialize the router instance
+  const pathname = usePathname();
 
   const getMe = async () => {
     try {
-      const token = Cookies.get('token');
-      const user = Cookies.get('userId');
+      // Check if localStorage is available
+      if (typeof localStorage !== 'undefined') {
+        const userId = localStorage.getItem('userId');
+        const token = localStorage.getItem('token');
 
-      if (!token && !user) {
-        setAuthenticated(false);
-        setLoading(false);
-        return;
-      } else {
-        setAuthenticated(true);
-        setLoading(false);
 
-        
-        // Check if the current route is the login or signup page
-        if (pathname==='/login'||'/signup') {
-          // Redirect to the home page ("/") if authenticated
-          window.location.href = "/"; 
+        if (!token || !userId) {
+          setAuthenticated(false);
+        } else {
+          setAuthenticated(true);
+
+          if (pathname === '/login' || pathname === '/signup') {
+            // Redirect to the home page ("/") if authenticated
+            window.location.href = '/';
+            return; // Return early to avoid the code below running
+          }
         }
-
-        return;
+      } else {
+        // Handle the case where localStorage is not available
+        console.error('localStorage is not available in this environment.');
       }
 
       setLoading(false);
-    } catch (err: any) {
+    } catch (err) {
       setError(err);
       setAuthenticated(false);
-      setLoading(false);
     }
   };
 
