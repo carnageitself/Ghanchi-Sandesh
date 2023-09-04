ded184d136fda3ab26dfc7c26f542b91a882afab
namansingh98 <naman.rathore@outlook.com>
2023-09-04 21:56:31 +0530

    summary page modifed

--- app/summary/page.tsx
+++ app/summary/page.tsx
@@ -4,13 +4,16 @@ import Image from "next/image";
 import { BsShare } from "react-icons/bs";
 import { MdOutlineLocationOn } from "react-icons/md";
 
-const summary= () => {
+const summary = () => {
   return (
     <>
-      <div className="bg-white m-5">
-        <div className="container mx-auto p-4">
+      <div className="bg-[#C01D47] h-[50vh] flex justify-center items-center w-full">
+        <h1 className="text-5xl text-white text-center relative -top-7  ">Heading of news</h1>{" "}
+      </div>
+      <div className="bg-[#EEEEEF] ">
+        <div className="w-9/12 mx-auto p-4">
           {/* <!-- Responsive Image Container --> */}
-          <div className="w-1/2 mx-auto relative">
+          <div className="w-full mx-auto -top-32 relative">
             <Image
               src={Img}
               alt="Image"
@@ -19,17 +22,19 @@ const summary= () => {
           </div>
 
           {/* <!-- Icons --> */}
-          <div className="flex justify-between mt-4">
+          <div className="flex justify-between mt-4 relative -top-28">
             <div className="flex items-center">
-              <MdOutlineLocationOn />
+              <span className="text-[#EC5D82]">Location</span>
+              <MdOutlineLocationOn size="20" />
             </div>
             <div className="flex items-center">
-              <BsShare />
+              <span className="text-[#EC5D82] pr-2">Share</span>
+              <BsShare size="20" />
             </div>
           </div>
 
           {/* <!-- Content Div --> */}
-          <div className="p-4 mt-4">
+          <div className="mt-4 relative -top-28">
             {/* <!-- Place your content here --> */}
             <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
             <p>
@@ -40,7 +45,7 @@ const summary= () => {
           </div>
 
           {/* <!-- Two Images with Responsive Layout --> */}
-          <div className="mt-4 flex justify-between">
+          <div className="mt-4 flex gap-4 justify-between relative -top-28">
             <div className="w-1/2">
               <Image src={Img} alt="Image 2" className="w-full h-auto" />
             </div>
